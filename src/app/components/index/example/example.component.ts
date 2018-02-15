51da89a135e5a6b7d7e3724599796b2b2b1c5009
Tiwakorn Janpanya <tiwakorn2507@gmail.com>
2018-02-15 16:34:20 +0700

    phai

--- src/app/components/index/example/example.component.ts
+++ src/app/components/index/example/example.component.ts
@@ -17,13 +17,22 @@ export class ExampleComponent implements OnInit {
   }
 
   validateForm(){
-    this.exampleForm = new FormGroup({
-      first_name: new FormControl(Validators.compose([Validators.required])),
-      last_name: new FormControl(Validators.compose([Validators.required])),
-      user_name: new FormControl(Validators.compose([Validators.required])),
-      city: new FormControl(Validators.compose([Validators.required])),
-      state: new FormControl(Validators.compose([Validators.required])),
-      zip: new FormControl(Validators.compose([Validators.required])),
-    });
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
   }
 }
