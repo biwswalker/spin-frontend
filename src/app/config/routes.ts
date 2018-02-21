500ea8621c6c7195ed2366c6c472345e28a79fc7
Biw Walker <biwswalker@Biws-MacBook-Pro.local>
2018-02-21 14:54:23 +0700

    Biwswalker

--- src/app/config/routes.ts
+++ src/app/config/routes.ts
@@ -7,14 +7,14 @@ import { AttendanceComponent } from '../components/attendance/attendance.compone
 import { TaskComponent } from '../components/attendance/task/task.component';
 
 const routes: Routes = [
-  { path: 'project', component: ProjectComponent},
+  { path: 'project', component: ProjectComponent },
   { path: 'attendance', component: AttendanceComponent },
-  { path: 'task', component: TaskComponent},
-  { path: 'example', component: ExampleComponent}
+  { path: 'task', component: TaskComponent },
+  { path: 'example', component: ExampleComponent }
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class RoutesModule { }
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class RoutesModule { }
