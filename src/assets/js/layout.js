c165e1b0f6db7c0c8b6a0abfbb2713b3bb03ac30
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-03-02 15:45:49 +0700

    Ig

--- src/assets/js/layout.js
+++ src/assets/js/layout.js
@@ -1,37 +1,26 @@
 $(document).ready(function () {
   var winddowsW = window.innerWidth; // New width
   menuDisplay(winddowsW);
-  layoutGapDisplay(winddowsW);
   $(window).resize(function () {
     // This will execute whenever the window is resized
     // var winddowsH = $(window).height(); // New height
     this.winddowsW = window.innerWidth; // New width
     menuDisplay(this.winddowsW);
-    layoutGapDisplay(this.winddowsW);
     // Date picker
     inlineDatepicker();
   });
 
-  $('#toggle-sidebar').on('click',function(){
+  $(document).on('click','#toggle-sidebar',function(){
     $('#sidebar').toggleClass('active');
   });
 
-  function layoutGapDisplay($width) {
-    if ($width > 1199 && $width < 1440) {
-      $('.menu-space').removeClass('col-xl-2');
-      $('.content-space').removeClass('col-xl-10');
-      $('.menu-space').addClass('col-xl-3');
-      $('.content-space').addClass('col-xl-9');
-    } else {
-      $('.menu-space').removeClass('col-xl-3');
-      $('.content-space').removeClass('col-xl-9');
-      $('.menu-space').addClass('col-xl-2');
-      $('.content-space').addClass('col-xl-10');
-    }
-  }
+  $(document).on('click','.spin-link ul li',function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
 
   function menuDisplay($width) {
-    if ($width > 767) {
+    if ($width > 576) {
       $('#sidebar').removeClass('active');
     }
 
