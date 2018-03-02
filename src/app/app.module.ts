c58606a190b6a150969120c9c3c4de97030220f1
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-03-02 12:08:40 +0700

    Ig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { CalcurationPipe } from './pipes/calcuration.pipe';
 // angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -24,8 +23,7 @@ import { ProjectModalComponent } from './components/project/project-modal/projec
 import { ProjectModalDetailComponent } from './components/project/project-modal/detail/project-modal-detail.component';
 import { ProjectModalPhaseComponent } from './components/project/project-modal/phase/project-modal-phase.component';
 import { ProjectModalMemberComponent } from './components/project/project-modal/member/project-modal-member.component';
-import { ExampleComponent } from './components/index/example/example.component';
-import { ComponentExampleComponent } from './components/index/component-example/component-example.component';
+import { ExampleComponent } from './components/example/example.component';
 import { AttendanceComponent } from './components/attendance/attendance.component';
 import { TimestampComponent } from './components/attendance/timestamp/timestamp.component';
 import { TaskComponent } from './components/attendance/task/task.component';
@@ -79,7 +77,6 @@ import { ProjectNamePipe } from './pipes/project-name.pipe';
     TaskComponent,
     TaskModalComponent,
     ExampleComponent,
-    ComponentExampleComponent,
     AttendanceComponent,
     TimetableDayComponent,
     TimetableWeekComponent,
@@ -96,7 +93,6 @@ import { ProjectNamePipe } from './pipes/project-name.pipe';
     PrjInfoSummaryComponent,
     ThaiDatePipe,
     ProjectNamePipe,
-    CalcurationPipe,
   ],
   imports: [
     BrowserModule,
