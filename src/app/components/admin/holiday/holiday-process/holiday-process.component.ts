1b5d4e6a8693b2e8b93c437ed34abb8a7f4cfefd
poteii <poteii.pasel@icloud.com>
2018-03-07 09:17:29 +0700

    by supreeya.ch

--- src/app/components/admin/holiday/holiday-process/holiday-process.component.ts
+++ src/app/components/admin/holiday/holiday-process/holiday-process.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Holiday } from '../../../../models/holiday';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-holiday-process',
@@ -8,11 +9,27 @@ import { Holiday } from '../../../../models/holiday';
 })
 export class HolidayProcessComponent implements OnInit {
 
-  protected holiday: Holiday = new Holiday();
+  public holiday: Holiday = new Holiday();
+  protected holidayGroup: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.holiday = new Holiday();
+    //  this.validateForm();
+  }
 
   ngOnInit() {
+    console.log('holiday = {}', this.holiday);
   }
+
+  // validateForm() {
+  //   this.holidayGroup = new FormGroup({
+  //     holName: new FormControl(this.holiday.holName, Validators.required),
+  //     holDate: new FormControl(this.holiday.holDate, Validators.required),
+  //     remark: new FormControl(this.holiday.remark),
+  //     activeFlag: new FormControl(this.holiday.activeFlag),
+  //     holId: new FormControl(this.holiday.holId),
+  //     versionId: new FormControl(this.holiday.versionId)
+  //   })
+  // }
 
 }
