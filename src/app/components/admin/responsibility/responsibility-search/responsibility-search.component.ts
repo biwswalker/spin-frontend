0c2d31e9368fe75f624c64ada8e74eaa22ed9134
poteii <poteii.pasel@icloud.com>
2018-03-09 15:59:20 +0700

    by poteii

--- src/app/components/admin/responsibility/responsibility-search/responsibility-search.component.ts
+++ src/app/components/admin/responsibility/responsibility-search/responsibility-search.component.ts
@@ -1,7 +1,13 @@
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { FormControl } from '@angular/forms';
 import { ResponsibilityService } from './../../../../providers/responsibility.service';
 import { Observable } from 'rxjs/Observable';
 import { Responsibility } from './../../../../models/responsibility';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { AsyncSubject } from 'rxjs/AsyncSubject';
+import 'rxjs/Rx';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'app-responsibility-search',
@@ -11,48 +17,96 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class ResponsibilitySearchComponent implements OnInit {
 
   public responsibilities: Observable<Responsibility[]>;
+  public oldResp: Observable<Responsibility[]>;
+  public resp: AsyncSubject<Responsibility[]>;
+  // public _responsibilities: BehaviorSubject<Responsibility[]>;
+  public _responsibilities: Observable<Responsibility[]>;
+  private loading: boolean = false;
 
-  @Output() respId = new EventEmitter<number>();
+  @Output() messageEvent = new EventEmitter<number>();
 
+  public page = 1;
+  public size = 11;
 
-  protected size: number = 0;
+  public throttle = 1000;
+  public scrollDistance = 1;
+  protected hasCriteria: boolean = false;
+  protected criteriaValue: string;
+  private searchField: FormControl;
 
   constructor(protected responsibilityService: ResponsibilityService) {
-
+    this._responsibilities = new BehaviorSubject<Responsibility[]>([]);
   }
 
   ngOnInit() {
     this.getAllResponsibility();
+    //   this.getNext();
   }
 
 
   getAllResponsibility() {
-    this.responsibilities = this.responsibilityService.findAll().do(
+    this.responsibilities = this.responsibilityService.findAllPageable(this.page, this.size).do(
       collection => {
-        this.size = collection.length;
         console.log(collection[0]);
-        this.respId.emit(collection[0].respId);
+        this.messageEvent.emit(collection[0].respId);
       }
     );
   }
 
   onChangeResp(resp) {
     console.log(resp);
-    this.respId.emit(resp.respId);
+    this.messageEvent.emit(resp.respId);
   }
 
-  onSearchByCriteria(key) {
-    console.log(key);
-    this.responsibilities = this.responsibilityService.findByCriteria(key).do(
-      collection => {
-        console.log(collection);
-        this.size = collection.length;
-        if(collection.length > 0){
-          this.respId.emit(collection[0].respId);
+  onSearchByCriteria(criteria) {
+    console.log(criteria);
+    if (criteria) {
+      this.criteriaValue = criteria;
+      this.responsibilities = this.responsibilityService.findByCriteria(criteria, this.page, this.size).do(
+        collection => {
+          console.log(collection);
+          if (collection.length > 0) {
+            this.messageEvent.emit(collection[0].respId);
+          }
         }
-      }
-    );
+      );
+    } else {
+      this.criteriaValue = '';
+      this.getAllResponsibility();
+    }
   }
 
+  // onScrollDown() {
+  //   console.log('onScrollDown');
+  //   this.responsibilityService.findAllPageable(this.page, this.size).do(
+  //     collection => {
+  //     }
+  //   );
+  //   this.page += 1;
+  // }
+
+
+  onScrollDown() {
+    console.log('onScrollDown');
+    this.oldResp = this.responsibilities;
+    console.log(this.oldResp);
+    this._responsibilities = this.getNext();
+    console.log(this._responsibilities);
+    //this.responsibilities = Observable.merge(this.oldResp, this._responsibilities);
+    if (this.oldResp) {
+      this.responsibilities = Observable.merge(this.oldResp, this._responsibilities);
+    }
+
+  }
+
+  getNext() {
+    let obs = this.responsibilityService.findAllPageable(this.page + 1, this.size);
+    obs.subscribe(
+      res => {
+        console.log(res);
+      });
+
+    return obs;
+  }
 
 }
