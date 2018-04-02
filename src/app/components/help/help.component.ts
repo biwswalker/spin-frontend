0933f22f5a49cb90c8e28435cc75e022767fa72a
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-04-02 14:27:47 +0700

    Ig

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -11,6 +11,16 @@ export class HelpComponent implements OnInit {
   highlightTrusted:SafeUrl=null;
   qrCode:string=null;
   qrCodeTrusted:SafeUrl=null;
+  helpImg2:SafeUrl=null;
+  helpImg3:SafeUrl=null;
+  helpImg4:SafeUrl=null;
+  helpImg5:SafeUrl=null;
+  helpImg6:SafeUrl=null;
+  helpImg7:SafeUrl=null;
+  helpImg8:SafeUrl=null;
+  helpImg9:SafeUrl=null;
+  helpImg10:SafeUrl=null;
+
   constructor( private sanitizer: DomSanitizer,) { }
 
   ngOnInit() {
@@ -19,6 +29,14 @@ export class HelpComponent implements OnInit {
 
     this.qrCode = './assets/img/help/28663.jpg';
     this.qrCodeTrusted = this.sanitizer.bypassSecurityTrustUrl(this.qrCode);
+    this.helpImg2 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg3 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg4 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg5 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg6 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg7 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg8 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg9 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
+    this.helpImg10 = this.sanitizer.bypassSecurityTrustUrl("./assets/img/help/Slide2.JPG");
   }
-
 }
