033beb3f6efbcb5a89e6731e66c42c8196b9a3c1
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-04-18 11:28:38 +0700

    Ig

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -1,10 +1,16 @@
 //For Project infinite scroll
-$(document).on('click','.data-can-select-list li,.search-results-body>div',function(event) {
+$(document).on('click','.data-can-select-list li',function(event) {
   var target = $(event.target);
  if(!target.is('.fa-heart')){
     $(this).addClass('active').siblings().removeClass('active');
     $('html,body').animate({ scrollTop: 0 }, 'slow');
  }
+});
+
+
+$(document).on('click','.search-results-body>div',function(event) {
+    $(this).addClass('active').siblings().removeClass('active');
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
 });
 
 var SpinCustomListUI = (function(){
